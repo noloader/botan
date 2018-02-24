f512f86f364b8988311c27ca598d5aa109787136
Jeffrey Walton <noloader@gmail.com>
2018-02-24 03:14:23 -0500

    Use 6x blocks for AES on Power8
    Here are the performance numbers. AES-128 and AES-256 benefit the most.
    
    GCC112, aes-ppc baseline 1x
    
    $ ./botan speed --msec=3000 AES-128
    AES-128 encrypt buffer size 1024 bytes: 1340.119 MiB/sec 0.37 cycles/byte (4020.36 MiB in 3000.00 ms)
    AES-128 decrypt buffer size 1024 bytes: 1607.275 MiB/sec 0.31 cycles/byte (4821.83 MiB in 3000.00 ms)
    
    GCC112, aes-ppc 6x
    
    $ ./botan speed --msec=3000 AES-128
    AES-128 encrypt buffer size 1024 bytes: 1756.595 MiB/sec 0.28 cycles/byte (5269.79 MiB in 3000.00 ms)
    AES-128 decrypt buffer size 1024 bytes: 1711.983 MiB/sec 0.29 cycles/byte (5135.95 MiB in 3000.00 ms)
    
    GCC112, aes-ppc 6x non-const src
    
    $ ./botan speed --msec=3000 AES-128
    AES-128 encrypt buffer size 1024 bytes: 1817.433 MiB/sec 0.27 cycles/byte (5452.30 MiB in 3000.00 ms)
    AES-128 decrypt buffer size 1024 bytes: 1894.507 MiB/sec 0.26 cycles/byte (5683.52 MiB in 3000.00 ms)
    
    **********
    
    GCC112, aes-ppc baseline 1x
    
    $ ./botan speed --msec=3000 AES-192
    AES-192 encrypt buffer size 1024 bytes: 1428.126 MiB/sec 0.35 cycles/byte (4284.38 MiB in 3000.00 ms)
    AES-192 decrypt buffer size 1024 bytes: 1407.650 MiB/sec 0.35 cycles/byte (4222.95 MiB in 3000.00 ms)
    
    GCC112, aes-ppc baseline 6x
    
    $ ./botan speed --msec=3000 AES-192
    AES-192 encrypt buffer size 1024 bytes: 1667.640 MiB/sec 0.30 cycles/byte (5002.92 MiB in 3000.00 ms)
    AES-192 decrypt buffer size 1024 bytes: 1680.232 MiB/sec 0.29 cycles/byte (5040.70 MiB in 3000.00 ms)
    
    **********
    
    GCC112, aes-ppc baseline 1x
    
    $ ./botan speed --msec=3000 AES-256
    AES-256 encrypt buffer size 1024 bytes: 1124.954 MiB/sec 0.44 cycles/byte (3374.86 MiB in 3000.00 ms)
    AES-256 decrypt buffer size 1024 bytes: 1128.737 MiB/sec 0.44 cycles/byte (3386.21 MiB in 3000.00 ms)
    
    $ ./botan speed --msec=3000 AES-256
    AES-256 encrypt buffer size 1024 bytes: 1518.324 MiB/sec 0.32 cycles/byte (4554.97 MiB in 3000.00 ms)
    AES-256 decrypt buffer size 1024 bytes: 1517.793 MiB/sec 0.32 cycles/byte (4553.38 MiB in 3000.00 ms)

--- src/lib/block/aes/aes_power8/aes_power8.cpp
+++ src/lib/block/aes/aes_power8/aes_power8.cpp
@@ -1,10 +1,7 @@
 /*
 * AES using POWER8 crypto extensions
 *
-* Contributed by Jeffrey Walton
-*
-* Further changes
-* (C) 2018 Jack Lloyd
+* Contributed by Jack Lloyd and Jeffrey Walton
 *
 * Botan is released under the Simplified BSD License (see license.txt)
 */
@@ -52,7 +49,8 @@ __vector unsigned char Reverse8x16(const __vector unsigned char src)
 
 __vector unsigned long long LoadBlock(const uint8_t* src)
    {
-   return (__vector unsigned long long)Reverse8x16(vec_vsx_ld(0, src));
+   // Note: cast away const-ness, profit 100 MiB/s.
+   return (__vector unsigned long long)Reverse8x16(vec_vsx_ld(0, (uint8_t*)src));
    }
 
 void StoreBlock(const __vector unsigned long long src, uint8_t* dest)
@@ -61,6 +59,61 @@ void StoreBlock(const __vector unsigned long long src, uint8_t* dest)
    }
 
 }
+
+#define AES_XOR_6_ROUNDS(K)      \
+   do                            \
+      {                          \
+      B0 = vec_xor(B0, K);       \
+      B1 = vec_xor(B1, K);       \
+      B2 = vec_xor(B2, K);       \
+      B3 = vec_xor(B3, K);       \
+      B4 = vec_xor(B4, K);       \
+      B5 = vec_xor(B5, K);       \
+      } while(0)
+
+#define AES_ENC_6_ROUNDS(K)                       \
+   do                                             \
+      {                                           \
+      B0 = __builtin_crypto_vcipher(B0, K);       \
+      B1 = __builtin_crypto_vcipher(B1, K);       \
+      B2 = __builtin_crypto_vcipher(B2, K);       \
+      B3 = __builtin_crypto_vcipher(B3, K);       \
+      B4 = __builtin_crypto_vcipher(B4, K);       \
+      B5 = __builtin_crypto_vcipher(B5, K);       \
+      } while(0)
+
+#define AES_ENC_6_LAST_ROUNDS(K)                  \
+   do                                             \
+      {                                           \
+      B0 = __builtin_crypto_vcipherlast(B0, K);   \
+      B1 = __builtin_crypto_vcipherlast(B1, K);   \
+      B2 = __builtin_crypto_vcipherlast(B2, K);   \
+      B3 = __builtin_crypto_vcipherlast(B3, K);   \
+      B4 = __builtin_crypto_vcipherlast(B4, K);   \
+      B5 = __builtin_crypto_vcipherlast(B5, K);   \
+      } while(0)
+
+#define AES_DEC_6_ROUNDS(K)                       \
+   do                                             \
+      {                                           \
+      B0 = __builtin_crypto_vncipher(B0, K);      \
+      B1 = __builtin_crypto_vncipher(B1, K);      \
+      B2 = __builtin_crypto_vncipher(B2, K);      \
+      B3 = __builtin_crypto_vncipher(B3, K);      \
+      B4 = __builtin_crypto_vncipher(B4, K);      \
+      B5 = __builtin_crypto_vncipher(B5, K);      \
+      } while(0)
+
+#define AES_DEC_6_LAST_ROUNDS(K)                  \
+   do                                             \
+      {                                           \
+      B0 = __builtin_crypto_vncipherlast(B0, K);  \
+      B1 = __builtin_crypto_vncipherlast(B1, K);  \
+      B2 = __builtin_crypto_vncipherlast(B2, K);  \
+      B3 = __builtin_crypto_vncipherlast(B3, K);  \
+      B4 = __builtin_crypto_vncipherlast(B4, K);  \
+      B5 = __builtin_crypto_vncipherlast(B5, K);  \
+      } while(0)
 
 BOTAN_FUNC_ISA("crypto")
 void AES_128::power8_encrypt_n(const uint8_t in[], uint8_t out[], size_t blocks) const
@@ -78,6 +131,40 @@ void AES_128::power8_encrypt_n(const uint8_t in[], uint8_t out[], size_t blocks)
    const __vector unsigned long long K8  = LoadKey(&m_EK[32]);
    const __vector unsigned long long K9  = LoadKey(&m_EK[36]);
    const __vector unsigned long long K10 = LoadBlock(m_ME.data());
+
+   while(blocks >= 6)
+      {
+      __vector unsigned long long B0 = LoadBlock(in+ 0);
+      __vector unsigned long long B1 = LoadBlock(in+16);
+      __vector unsigned long long B2 = LoadBlock(in+32);
+      __vector unsigned long long B3 = LoadBlock(in+48);
+      __vector unsigned long long B4 = LoadBlock(in+64);
+      __vector unsigned long long B5 = LoadBlock(in+80);
+
+      AES_XOR_6_ROUNDS(K0);
+
+      AES_ENC_6_ROUNDS(K1);
+      AES_ENC_6_ROUNDS(K2);
+      AES_ENC_6_ROUNDS(K3);
+      AES_ENC_6_ROUNDS(K4);
+      AES_ENC_6_ROUNDS(K5);
+      AES_ENC_6_ROUNDS(K6);
+      AES_ENC_6_ROUNDS(K7);
+      AES_ENC_6_ROUNDS(K8);
+      AES_ENC_6_ROUNDS(K9);
+      AES_ENC_6_LAST_ROUNDS(K10);
+
+      StoreBlock(B0, out+ 0);
+      StoreBlock(B1, out+16);
+      StoreBlock(B2, out+32);
+      StoreBlock(B3, out+48);
+      StoreBlock(B4, out+64);
+      StoreBlock(B5, out+80);
+
+      blocks -= 6;
+      in  += 6*16;
+      out += 6*16;
+      }
 
    for(size_t i = 0; i != blocks; ++i)
       {
@@ -118,6 +205,40 @@ void AES_128::power8_decrypt_n(const uint8_t in[], uint8_t out[], size_t blocks)
    const __vector unsigned long long K8  = LoadKey(&m_EK[8]);
    const __vector unsigned long long K9  = LoadKey(&m_EK[4]);
    const __vector unsigned long long K10 = LoadKey(&m_EK[0]);
+
+   while(blocks >= 6)
+      {
+      __vector unsigned long long B0 = LoadBlock(in+ 0);
+      __vector unsigned long long B1 = LoadBlock(in+16);
+      __vector unsigned long long B2 = LoadBlock(in+32);
+      __vector unsigned long long B3 = LoadBlock(in+48);
+      __vector unsigned long long B4 = LoadBlock(in+64);
+      __vector unsigned long long B5 = LoadBlock(in+80);
+
+      AES_XOR_6_ROUNDS(K0);
+
+      AES_DEC_6_ROUNDS(K1);
+      AES_DEC_6_ROUNDS(K2);
+      AES_DEC_6_ROUNDS(K3);
+      AES_DEC_6_ROUNDS(K4);
+      AES_DEC_6_ROUNDS(K5);
+      AES_DEC_6_ROUNDS(K6);
+      AES_DEC_6_ROUNDS(K7);
+      AES_DEC_6_ROUNDS(K8);
+      AES_DEC_6_ROUNDS(K9);
+      AES_DEC_6_LAST_ROUNDS(K10);
+
+      StoreBlock(B0, out+ 0);
+      StoreBlock(B1, out+16);
+      StoreBlock(B2, out+32);
+      StoreBlock(B3, out+48);
+      StoreBlock(B4, out+64);
+      StoreBlock(B5, out+80);
+
+      blocks -= 6;
+      in  += 6*16;
+      out += 6*16;
+      }
 
    for(size_t i = 0; i != blocks; ++i)
       {
@@ -160,6 +281,42 @@ void AES_192::power8_encrypt_n(const uint8_t in[], uint8_t out[], size_t blocks)
    const __vector unsigned long long K10 = LoadKey(&m_EK[40]);
    const __vector unsigned long long K11 = LoadKey(&m_EK[44]);
    const __vector unsigned long long K12 = LoadBlock(m_ME.data());
+
+   while(blocks >= 6)
+      {
+      __vector unsigned long long B0 = LoadBlock(in+ 0);
+      __vector unsigned long long B1 = LoadBlock(in+16);
+      __vector unsigned long long B2 = LoadBlock(in+32);
+      __vector unsigned long long B3 = LoadBlock(in+48);
+      __vector unsigned long long B4 = LoadBlock(in+64);
+      __vector unsigned long long B5 = LoadBlock(in+80);
+
+      AES_XOR_6_ROUNDS(K0);
+
+      AES_ENC_6_ROUNDS(K1);
+      AES_ENC_6_ROUNDS(K2);
+      AES_ENC_6_ROUNDS(K3);
+      AES_ENC_6_ROUNDS(K4);
+      AES_ENC_6_ROUNDS(K5);
+      AES_ENC_6_ROUNDS(K6);
+      AES_ENC_6_ROUNDS(K7);
+      AES_ENC_6_ROUNDS(K8);
+      AES_ENC_6_ROUNDS(K9);
+      AES_ENC_6_ROUNDS(K10);
+      AES_ENC_6_ROUNDS(K11);
+      AES_ENC_6_LAST_ROUNDS(K12);
+
+      StoreBlock(B0, out+ 0);
+      StoreBlock(B1, out+16);
+      StoreBlock(B2, out+32);
+      StoreBlock(B3, out+48);
+      StoreBlock(B4, out+64);
+      StoreBlock(B5, out+80);
+
+      blocks -= 6;
+      in  += 6*16;
+      out += 6*16;
+      }
 
    for(size_t i = 0; i != blocks; ++i)
       {
@@ -205,6 +362,42 @@ void AES_192::power8_decrypt_n(const uint8_t in[], uint8_t out[], size_t blocks)
    const __vector unsigned long long K11 = LoadKey(&m_EK[4]);
    const __vector unsigned long long K12 = LoadKey(&m_EK[0]);
 
+   while(blocks >= 6)
+      {
+      __vector unsigned long long B0 = LoadBlock(in+ 0);
+      __vector unsigned long long B1 = LoadBlock(in+16);
+      __vector unsigned long long B2 = LoadBlock(in+32);
+      __vector unsigned long long B3 = LoadBlock(in+48);
+      __vector unsigned long long B4 = LoadBlock(in+64);
+      __vector unsigned long long B5 = LoadBlock(in+80);
+
+      AES_XOR_6_ROUNDS(K0);
+
+      AES_DEC_6_ROUNDS(K1);
+      AES_DEC_6_ROUNDS(K2);
+      AES_DEC_6_ROUNDS(K3);
+      AES_DEC_6_ROUNDS(K4);
+      AES_DEC_6_ROUNDS(K5);
+      AES_DEC_6_ROUNDS(K6);
+      AES_DEC_6_ROUNDS(K7);
+      AES_DEC_6_ROUNDS(K8);
+      AES_DEC_6_ROUNDS(K9);
+      AES_DEC_6_ROUNDS(K10);
+      AES_DEC_6_ROUNDS(K11);
+      AES_DEC_6_LAST_ROUNDS(K12);
+
+      StoreBlock(B0, out+ 0);
+      StoreBlock(B1, out+16);
+      StoreBlock(B2, out+32);
+      StoreBlock(B3, out+48);
+      StoreBlock(B4, out+64);
+      StoreBlock(B5, out+80);
+
+      blocks -= 6;
+      in  += 6*16;
+      out += 6*16;
+      }
+
    for(size_t i = 0; i != blocks; ++i)
       {
       __vector unsigned long long B = LoadBlock(in);
@@ -249,6 +442,44 @@ void AES_256::power8_encrypt_n(const uint8_t in[], uint8_t out[], size_t blocks)
    const __vector unsigned long long K12 = LoadKey(&m_EK[48]);
    const __vector unsigned long long K13 = LoadKey(&m_EK[52]);
    const __vector unsigned long long K14 = LoadBlock(m_ME.data());
+
+   while(blocks >= 6)
+      {
+      __vector unsigned long long B0 = LoadBlock(in+ 0);
+      __vector unsigned long long B1 = LoadBlock(in+16);
+      __vector unsigned long long B2 = LoadBlock(in+32);
+      __vector unsigned long long B3 = LoadBlock(in+48);
+      __vector unsigned long long B4 = LoadBlock(in+64);
+      __vector unsigned long long B5 = LoadBlock(in+80);
+
+      AES_XOR_6_ROUNDS(K0);
+
+      AES_ENC_6_ROUNDS(K1);
+      AES_ENC_6_ROUNDS(K2);
+      AES_ENC_6_ROUNDS(K3);
+      AES_ENC_6_ROUNDS(K4);
+      AES_ENC_6_ROUNDS(K5);
+      AES_ENC_6_ROUNDS(K6);
+      AES_ENC_6_ROUNDS(K7);
+      AES_ENC_6_ROUNDS(K8);
+      AES_ENC_6_ROUNDS(K9);
+      AES_ENC_6_ROUNDS(K10);
+      AES_ENC_6_ROUNDS(K11);
+      AES_ENC_6_ROUNDS(K12);
+      AES_ENC_6_ROUNDS(K13);
+      AES_ENC_6_LAST_ROUNDS(K14);
+
+      StoreBlock(B0, out+ 0);
+      StoreBlock(B1, out+16);
+      StoreBlock(B2, out+32);
+      StoreBlock(B3, out+48);
+      StoreBlock(B4, out+64);
+      StoreBlock(B5, out+80);
+
+      blocks -= 6;
+      in  += 6*16;
+      out += 6*16;
+      }
 
    for(size_t i = 0; i != blocks; ++i)
       {
@@ -297,6 +528,44 @@ void AES_256::power8_decrypt_n(const uint8_t in[], uint8_t out[], size_t blocks)
    const __vector unsigned long long K12 = LoadKey(&m_EK[8]);
    const __vector unsigned long long K13 = LoadKey(&m_EK[4]);
    const __vector unsigned long long K14 = LoadKey(&m_EK[0]);
+
+   while(blocks >= 6)
+      {
+      __vector unsigned long long B0 = LoadBlock(in+ 0);
+      __vector unsigned long long B1 = LoadBlock(in+16);
+      __vector unsigned long long B2 = LoadBlock(in+32);
+      __vector unsigned long long B3 = LoadBlock(in+48);
+      __vector unsigned long long B4 = LoadBlock(in+64);
+      __vector unsigned long long B5 = LoadBlock(in+80);
+
+      AES_XOR_6_ROUNDS(K0);
+
+      AES_DEC_6_ROUNDS(K1);
+      AES_DEC_6_ROUNDS(K2);
+      AES_DEC_6_ROUNDS(K3);
+      AES_DEC_6_ROUNDS(K4);
+      AES_DEC_6_ROUNDS(K5);
+      AES_DEC_6_ROUNDS(K6);
+      AES_DEC_6_ROUNDS(K7);
+      AES_DEC_6_ROUNDS(K8);
+      AES_DEC_6_ROUNDS(K9);
+      AES_DEC_6_ROUNDS(K10);
+      AES_DEC_6_ROUNDS(K11);
+      AES_DEC_6_ROUNDS(K12);
+      AES_DEC_6_ROUNDS(K13);
+      AES_DEC_6_LAST_ROUNDS(K14);
+
+      StoreBlock(B0, out+ 0);
+      StoreBlock(B1, out+16);
+      StoreBlock(B2, out+32);
+      StoreBlock(B3, out+48);
+      StoreBlock(B4, out+64);
+      StoreBlock(B5, out+80);
+
+      blocks -= 6;
+      in  += 6*16;
+      out += 6*16;
+      }
 
    for(size_t i = 0; i != blocks; ++i)
       {
